c48c3922095cd282ef4bb7140e8860e6cc4f0a92
Markus Kaller <markus_kaller@gmx.de>
2024-12-15 16:58:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-let taxiFareApiUrl = 'https://taxifare-1082554174077.europe-west1.run.app'; // replace with your API endpoint
+let taxiFareApiUrl = 'https://taxifare-1082554174077.europe-west1.run.app/predict'; // replace with your API endpoint
 const centralCoordinates = [-74.00597, 40.71427]; // starting position [lng, lat]
 
 if (window.location.href.includes('https://mkaller76.github.io/taxi-fare-interface/')) {
-  taxiFareApiUrl = 'https://taxifare-1082554174077.europe-west1.run.app';
+  taxiFareApiUrl = 'https://taxifare-1082554174077.europe-west1.run.app/predict';
 }
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
